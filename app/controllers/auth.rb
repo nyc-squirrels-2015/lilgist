84d5ad6fb92176b73b5ca61bbbd553465ec6ff19
Alex Homer <realalexhomer@gmail.com>
2015-02-18 13:51:06 -0500

    makes site look nice, gets jquery login/signup forms

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -2,10 +2,31 @@ get '/' do
   erb :welcome
 end
 
-get '/login' do
-  
+get '/authenticate/user/failure' do
+    @error = true
+    erb :welcome
 end
 
-get '/signup' do
-  
+post '/authenticate/signup.json' do
+  content_type :json
+  user = User.create(params[:user])
+  user.to_json
+end
+
+
+
+post '/authenticate/user' do
+  @error = false
+  user = User.find_by_name(params[:name])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.user_id
+    redirect '/'
+  else
+    redirect '/authenticate/user/failure'
+  end
+end
+
+get '/authenticate/logout.json' do
+  session[:user_id] = nil
+  200
 end
---
+++ public/js/views/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#login-link').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
+
+
+  $('#signup-link').click(function (event) {
+    event.preventDefault();
+    $('#template-container').loadTemplate($('#signup'))
+  });
+});
